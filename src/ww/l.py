8995b01d19d9d91611e549f61385d6b60133304c
lucien <luciendartois@gmail.com>
2016-10-13 16:28:59 +0200

    Add basic support for list wrapper

--- src/ww/l.py
+++ src/ww/l.py
@@ -3,15 +3,65 @@
 
 class l(list):
 
+    @property
     def len(self):
+        """Return len(self)
+            
+        Example:
+
+            >>> list = [0, 1, 2, 3]
+            >>> list.len
+            4
+        """
+        
         return len(self)
 
     def join(self, iterable, formatter=lambda s, t: t.format(s), template="{}"):
+    
         return  s.join(iterable, formatter, template)
 
     def append(self, *values):
+        """Append values at the end of the list
+        
+        Allow chaining.
+
+        Args:
+            values: values to be appened at the end.
+
+        Example:
+
+            >>> lst = l([])
+            >>> lst.append(1)
+            >>> lst
+            [1]
+            >>> lst.append(2, 3).append(4,5)
+            >>> lst
+            [1, 2, 3, 4, 5]
+        """
+        
         for value in values:
-            self.append(value)
-
-
+            list.append(self, value)
+        return self
     # todo : remplement slicing like g()
+    
+    def extend(self, *values):
+        """Chaining Extend *values on self
+
+        Args:
+            *values: iterable to use for the iner state.
+
+        Example:
+
+            >>> lst = l([])
+            >>> lst.extend([1, 2])
+            >>> lst
+            [1, 2]
+            >>> lst.extend([3, 4]).extend([5, 6])
+            >>> lst
+            [1, 2, 3, 4, 5, 6]
+        """
+        
+        for value in values:
+            list.extend(self, value)
+        return self
+    
